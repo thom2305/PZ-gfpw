3753864b364501bce4b84d738e1bc8482b179a2f
thom2305 <102167733+thom2305@users.noreply.github.com>
2023-10-16 19:50:43 -0400

    empty line rem

--- media/lua/client/WaterPowerGenerator.lua
+++ media/lua/client/WaterPowerGenerator.lua
@@ -2,18 +2,17 @@ wpg = wpg or {};
 
 --localfunctions/helpers
 
-
 --events
 
 --main function
 
-function WaterPowerGenerator:toggle(which, state)
+function wpg:toggle(which, state)
     local options = SandboxOptions.new();
 
     if state == "on" or state == "off"
     then
         options:copyValuesFrom(getSandboxOptions());
-        options:getOptionByName(WaterPowerGenerator:getOptionName(which)):setValue(state == "on"
+        options:getOptionByName(wpg:getOptionName(which)):setValue(state == "on"
             and 2147483647
             or -1
         );
